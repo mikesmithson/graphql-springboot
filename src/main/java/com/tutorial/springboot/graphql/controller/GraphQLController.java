cba794254489757ad94a57b83d0ab4af4e225ee0
Mike Smithson <mikesmithso@gmail.com>
2024-02-19 15:58:07 -0500

    use batch mapping in place of schema mapping

--- src/main/java/com/tutorial/springboot/graphql/controller/GraphQLController.java
+++ src/main/java/com/tutorial/springboot/graphql/controller/GraphQLController.java
@@ -6,11 +6,13 @@ import com.tutorial.springboot.graphql.service.MemberService;
 import com.tutorial.springboot.graphql.service.ResultService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.BatchMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,20 +22,21 @@ public class GraphQLController {
 
     @QueryMapping
     public String firstQuery() {
-        return  "First GQL Query works";
+        return "First GQL Query works";
     }
 
     @QueryMapping
     public String secondQuery(@Argument String firstName, @Argument String lastName) {
-        return String.join(" ","Hello there,", firstName,lastName);
+        return String.join(" ", "Hello there,", firstName, lastName);
     }
+
     @QueryMapping
     public List<StudentResponse> getAllStudents() {
         return memberService.getAllStudents();
     }
 
-    @SchemaMapping(typeName = "StudentResponse", field = "result")
-    public List<StudentSubjectResponse> getResultsForStudents(StudentResponse studentResponse) {
-        return resultService.getResultForStudent(studentResponse.getId());
+    @BatchMapping(typeName = "StudentResponse", field = "result", maxBatchSize = 10)
+    public Map<StudentResponse, List<StudentSubjectResponse>> getResultsAllStudents(List<StudentResponse> studentResponses) {
+        return resultService.getResultForAllStudents(studentResponses);
     }
 }
--- src/main/java/com/tutorial/springboot/graphql/service/ResultService.java
+++ src/main/java/com/tutorial/springboot/graphql/service/ResultService.java
@@ -1,12 +1,15 @@
 package com.tutorial.springboot.graphql.service;
 
+import com.tutorial.springboot.graphql.entity.Result;
 import com.tutorial.springboot.graphql.repository.ResultRepository;
+import com.tutorial.springboot.graphql.response.StudentResponse;
 import com.tutorial.springboot.graphql.response.StudentSubjectResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.StreamSupport;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +25,23 @@ public class ResultService {
                         .marks(result.getMarks())
                         .build())
                 .toList();
+    }
+
+    public Map<StudentResponse, List<StudentSubjectResponse>> getResultForAllStudents(List<StudentResponse> studentResponses) {
+        log.debug("fetching all responses");
+        List<Result> results = StreamSupport.stream(repository.findAll().spliterator(), false).toList();
+        Map<StudentResponse, List<StudentSubjectResponse>> batchMap = new LinkedHashMap<>();
+
+        studentResponses.forEach(studentResponse -> {
+                    List<StudentSubjectResponse> studentSubjectResponses = results.stream()
+                            .filter(result -> studentResponse.getId() == result.getStudent().getId())
+                            .map(result -> StudentSubjectResponse.builder()
+                                    .marks(result.getMarks())
+                                    .subjectName(result.getSubject().getSubjectName())
+                                    .build()).toList();
+                    batchMap.put(studentResponse, studentSubjectResponses);
+                }
+        );
+        return batchMap;
     }
 }

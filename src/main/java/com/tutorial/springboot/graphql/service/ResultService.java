56329113236177c995d9ba75eb5f2288b58dc67c
Mike Smithson <mikesmithso@gmail.com>
2024-02-25 17:32:34 -0500

    more refactorings on ResultService

--- src/main/java/com/tutorial/springboot/graphql/service/ResultService.java
+++ src/main/java/com/tutorial/springboot/graphql/service/ResultService.java
@@ -4,10 +4,7 @@ import com.tutorial.springboot.graphql.entity.Result;
 import com.tutorial.springboot.graphql.entity.Subject;
 import com.tutorial.springboot.graphql.repository.ResultRepository;
 import com.tutorial.springboot.graphql.repository.SubjectRepository;
-import com.tutorial.springboot.graphql.response.MemberResponse;
-import com.tutorial.springboot.graphql.response.MemberSearchResult;
-import com.tutorial.springboot.graphql.response.StudentSubjectResponse;
-import com.tutorial.springboot.graphql.response.TeacherSubjectResponse;
+import com.tutorial.springboot.graphql.response.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -27,56 +24,56 @@ public class ResultService {
 
     public Map<MemberResponse, List<?>> getMemberResponseForAllStudents(List<MemberResponse> memberResponses) {
         log.debug("fetching all student responses");
-        List<Result> results = StreamSupport.stream(resultRepository.findAll().spliterator(), false).toList();
+        List<Result> results = fetchAllResults();
         return memberResponses.stream()
                 .collect(Collectors.toMap(memberResponse ->  memberResponse, memberResponse -> createStudentSubjectResponses(results, memberResponse.getId())));
     }
 
     public Map<MemberSearchResult, List<?>> getMemberSearchResultsForAllStudents(List<MemberSearchResult> studentResponses) {
         log.debug("fetching all student search results");
-        List<Result> results = StreamSupport.stream(resultRepository.findAll().spliterator(), false).toList();
+        List<Result> results = fetchAllResults();
 
         return studentResponses.stream()
                 .collect(Collectors.toMap(studentResponse -> studentResponse, studentResponse -> createStudentSubjectResponses(results, studentResponse.getId())));
     }
 
     public Map<MemberSearchResult, List<?>> getMemberSearchResultsForAllTeachers(List<MemberSearchResult> teacherResponses) {
-        log.debug("fetching all student search results");
-        List<Subject> results = StreamSupport.stream(subjectRepository.findAll().spliterator(), false).toList();
-        Map<MemberSearchResult, List<?>> batchMap = new LinkedHashMap<>();
+        log.debug("fetching all student search subjects");
+        List<Subject> subjects = fetchAllSubjects();
 
-        teacherResponses.forEach(teacherResponse -> {
-            List<TeacherSubjectResponse> teacherSubjectResponses = results.stream().filter(result -> teacherResponse.getId() == result.getTeacher().getId()).map(result -> {
-                TeacherSubjectResponse teacherSubjectResponse = new TeacherSubjectResponse();
-                teacherSubjectResponse.setSubjectName(result.getSubjectName());
-                teacherSubjectResponse.setExperience(result.getExperience());
-                return teacherSubjectResponse;
-            }).toList();
-
-            batchMap.put(teacherResponse, teacherSubjectResponses);
-        });
-        return batchMap;
+        return teacherResponses.stream()
+                .collect(Collectors
+                        .toMap(teacherResponse -> teacherResponse, teacherResponse -> createTeacherSubjectResponses(subjects, teacherResponse.getId())));
     }
 
     public Map<MemberResponse, List<?>> getMemberResponseForAllTeachers(List<MemberResponse> memberResponses) {
         log.debug("fetching all teachers");
-        List<Subject> subjects = StreamSupport.stream(subjectRepository.findAll().spliterator(), false).toList();
-        Map<MemberResponse, List<?>> batchMap = new LinkedHashMap<>();
+        List<Subject> subjects = fetchAllSubjects();
 
-        memberResponses.forEach(memberResponse -> {
-            List<TeacherSubjectResponse> teacherSubjectResponses = subjects.stream().filter(subject -> memberResponse.getId() == subject.getTeacher().getId()).map(subject -> {
-                TeacherSubjectResponse response = new TeacherSubjectResponse();
-                response.setExperience(subject.getExperience());
-                response.setSubjectName(subject.getSubjectName());
-                return response;
-            }).toList();
-            batchMap.put(memberResponse, teacherSubjectResponses);
-        });
-        return batchMap;
+        return memberResponses.stream()
+                .collect(Collectors
+                        .toMap(memberResponse-> memberResponse, memberResponse -> createTeacherSubjectResponses(subjects, memberResponse.getId())));
     }
 
-    private static List<StudentSubjectResponse> createStudentSubjectResponses(List<Result> results, int memberResponseId) {
-        return results.stream().filter(result -> memberResponseId == result.getStudent().getId()).map(result -> {
+    private List<Subject> fetchAllSubjects() {
+        return StreamSupport.stream(subjectRepository.findAll().spliterator(), false).toList();
+    }
+
+    private List<Result> fetchAllResults() {
+        return StreamSupport.stream(resultRepository.findAll().spliterator(), false).toList();
+    }
+
+    private static List<TeacherSubjectResponse> createTeacherSubjectResponses(List<Subject> subjects, Integer memberId) {
+        return subjects.stream().filter(subject -> memberId == subject.getTeacher().getId()).map(subject -> {
+                TeacherSubjectResponse teacherSubjectResponse = new TeacherSubjectResponse();
+                teacherSubjectResponse.setExperience(subject.getExperience());
+                teacherSubjectResponse.setSubjectName(subject.getSubjectName());
+                return teacherSubjectResponse;
+            }).toList();
+    }
+
+    private static List<StudentSubjectResponse> createStudentSubjectResponses(List<Result> results, int memberId) {
+        return results.stream().filter(result -> memberId == result.getStudent().getId()).map(result -> {
             StudentSubjectResponse studentSubjectResponse = new StudentSubjectResponse();
             studentSubjectResponse.setMarks(result.getMarks());
             studentSubjectResponse.setSubjectName(result.getSubject().getSubjectName());
